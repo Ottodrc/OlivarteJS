d424c9bdf169f650c06b5e2dd6c1e330f12215ce
Ottodrc <baratriker@gmail.com>
2021-10-09 17:47:46 -0300

    nuevo desafio

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class Producto {
     this.stock = stock;
     this.id = parseInt(id);
     this.precio;
-    this.vendido;
+    this.vendido = 0;
   }
 
   aumentarStock(numero){
@@ -14,7 +14,13 @@ class Producto {
         this.stock = this.stock + numero
 
   }
-  vender() {
+  agregarAlCarro(){
+    let carrito = [];
+
+
+  }
+  vender(cantidad) {
+    cantidad = parseInt(cantidad)
     if (this.tipo == "ACEITUNAS" && this.tamanio == "GRANDE") {
       this.precio = 350;
     } else if (this.tipo == "ACEITUNAS" && this.tamanio == "CHICO") {
@@ -23,101 +29,76 @@ class Producto {
       this.precio = 400;
     }
 
-    if (this.stock != 0) {
-      this.vendido = true;
-      this.stock -= 1;
-
-      //agregando etiqueta
-      let parrafoVenta = document.createElement("p");
-      parrafoVenta.innerHTML = `<p id="parrafoVenta">Hola, gracias por comprar ${this.tipo.toLowerCase()} ${this.nombre.toLowerCase()} el precio total es de $${this.precio} </p>`;
-      return document.getElementById("precioFinal").appendChild(parrafoVenta);
+    if (this.stock > cantidad ) {
+      
+      this.stock = (this.stock - 1)*cantidad;
+        $("#precioFinal").empty()
+       $("#precioFinal").prepend(`<p id="parrafoVenta">Hola, gracias por comprar ${this.tipo.toLowerCase()} ${this.nombre.toLowerCase()} el precio total es de $${this.precio} </p> x ${cantidad}`);
     } else {
-      return "Lo sentimos, no hay stock";
+        swal("Lo sentimos!", `actualmente no contamos con stock en el producto ${this.tipo.toLowerCase()} ${this.nombre.toLowerCase()}`,{
+          icon: "error",
+          button: true,
+        })
     }
   }
 }
 const productos = [];
   //Aceitunas comunes chicas
-productos.push(new Producto("aceitunas", "roquefort", "chico", "1", 20));
-productos.push(new Producto("aceitunas", "holanda", "chico", "2", 20));
-productos.push(new Producto("aceitunas", "cheddar", "chico", "3", 20));
-productos.push(new Producto("aceitunas", "crudo", "chico", "4", 20));
-productos.push(new Producto("aceitunas", "salame", "chico", "5", 20));
-productos.push(new Producto("aceitunas", "mix clasico", "chico", "6", 20));
-productos.push(new Producto("aceitunas", "mix quesos", "chico", "7", 20));
-productos.push(new Producto("aceitunas", "mix picada", "chico", "8", 20));
+productos.push(new Producto("aceitunas", "roquefort", "chico", "1", 3));
+productos.push(new Producto("aceitunas", "holanda", "chico", "2", 3));
+productos.push(new Producto("aceitunas", "cheddar", "chico", "3", 3));
+productos.push(new Producto("aceitunas", "crudo", "chico", "4", 3));
+productos.push(new Producto("aceitunas", "salame", "chico", "5", 3));
+productos.push(new Producto("aceitunas", "mix clasico", "chico", "6", 3));
+productos.push(new Producto("aceitunas", "mix quesos", "chico", "7", 3));
+productos.push(new Producto("aceitunas", "mix picada", "chico", "8", 3));
 
 //aceitunas comunes grandes
-productos.push(new Producto("aceitunas", "roquefort", "grande", "9", 20));
-productos.push(new Producto("aceitunas", "holanda", "grande", "10", 20));
-productos.push(new Producto("aceitunas", "cheddar", "grande", "11", 20));
-productos.push(new Producto("aceitunas", "crudo", "grande", "12", 20));
-productos.push(new Producto("aceitunas", "salame", "grande", "13", 20));
-productos.push(new Producto("aceitunas", "mix clasico", "grande", "14", 20));
-productos.push(new Producto("aceitunas", "mix quesos", "grande", "15", 20));
-productos.push(new Producto("aceitunas", "mix picada", "grande", "16", 20));
+productos.push(new Producto("aceitunas", "roquefort", "grande", "9", 3));
+productos.push(new Producto("aceitunas", "holanda", "grande", "10", 3));
+productos.push(new Producto("aceitunas", "cheddar", "grande", "11", 3));
+productos.push(new Producto("aceitunas", "crudo", "grande", "12", 3));
+productos.push(new Producto("aceitunas", "salame", "grande", "13", 3));
+productos.push(new Producto("aceitunas", "mix clasico", "grande", "14", 3));
+productos.push(new Producto("aceitunas", "mix quesos", "grande", "15", 3));
+productos.push(new Producto("aceitunas", "mix picada", "grande", "16", 3));
 
 //aceitunas comunes deluxe (no vienen grandes)
-productos.push(new Producto("aceitunas", "almendras", "chico", "17", 20));
-productos.push(new Producto("aceitunas", "capresse", "chico", "18", 20));
-productos.push(new Producto("aceitunas", "palmitos", "chico", "19", 20));
-productos.push(new Producto("aceitunas", "ajo", "chico", "20", 20));
+productos.push(new Producto("aceitunas", "almendras", "chico", "17", 3));
+productos.push(new Producto("aceitunas", "capresse", "chico", "18", 3));
+productos.push(new Producto("aceitunas", "palmitos", "chico", "19", 3));
+productos.push(new Producto("aceitunas", "ajo", "chico", "20", 3));
 
 //quesos
-productos.push(new Producto("quesos", "picante", "", "21", 20));
-productos.push(new Producto("quesos", "pimienta", "", "22", 20));
-productos.push(new Producto("quesos", "mediterraneo", "", "23", 20));  
+productos.push(new Producto("quesos", "picante", "", "21", 3));
+productos.push(new Producto("quesos", "pimienta", "", "22", 3));
+productos.push(new Producto("quesos", "mediterraneo", "", "23", 3));  
 
 //almacenar elementos iniciales con JSON + guardarlos en el storage
 localStorage.setItem("productos",JSON.stringify(productos))
 
 
-//evaluar lo guardado
-for (let index = 0; index < localStorage.length; index++) {
-    let clave = localStorage.key(index)
-    console.log("clave:" + clave)
-    console.log(localStorage.getItem(clave))
-}
-
-//recuperar los datos y trbajar con ellos
-let almacenados = [];
-almacenados = JSON.parse(localStorage.getItem("productos"));
-almacenados = productos
-for (const iterator of almacenados) {
-    iterator.aumentarStock(3)
-    localStorage.setItem("productos",JSON.stringify(almacenados))
-}
-
-
-
-
-
-
-
-
-
 
 //captura de envento submit
-let miForm = document.getElementById("producto");
 
-miForm.addEventListener("submit", validarForm);
-function validarForm(e) {
+
+$("#producto").submit(function (e) {
   e.preventDefault();
-  var formulario = e.target;
-  var valorRelleno = formulario.children[1].value;
+  let hijos = $(e.target).children();
+  var valorRelleno = hijos[1].value;
   const primerFiltro = productos.filter(
     (relleno) => relleno.nombre == valorRelleno.toUpperCase()
   );
-    var valorTamanio = formulario.children[3].value;
+    var valorTamanio =  hijos[3].value;
+    var valorCant = hijos[5].value
 const segundoFiltro = primerFiltro.filter(medida => medida.tamanio == valorTamanio.toUpperCase())
   if (segundoFiltro.length > 0) {
     for (const i of segundoFiltro) {
-      i.vender();
+      i.vender(valorCant);
     }
   } else {
     console.log("algo salio mal");
   }
 }
-
-
+);
 
